a3e1d94f02baa113811e327bb5db0147058e8875
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-18 16:26:29 +0600

    Added API for doctor

--- index.js
+++ index.js
@@ -44,6 +44,21 @@ async function run() {
         const doctorCollection = client.db('doctors_portal').collection('doctors');
         const reviewsCollection = client.db('patient_reviews').collection('reviews');
 
+        const verifyAdmin = async (req, res, next) => {
+
+            const requester = req.decoded.email;
+
+            const requesterAccount = await userCollection.findOne({ email: requester });
+
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'forbidden' });
+            }
+
+        }
+
         app.get('/service', async (req, res) => {
 
             const q = {};
@@ -157,13 +172,9 @@ async function run() {
 
         });
 
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
 
             const email = req.params.email;
-
-            const requester = req.decoded.email;
-
-            const requestedAccount = await userCollection.findOne({ email: requester });
 
             if (requestedAccount.role === 'admin') {
 
@@ -197,11 +208,31 @@ async function run() {
 
         });
 
-        app.post('/doctor', async (req, res) => {
+        app.post('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
 
             const doctor = req.body;
 
             const result = await doctorCollection.insertOne(doctor);
+
+            res.send(result);
+
+        });
+
+        app.get('/doctor', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const doctors = await doctorCollection.find().toArray();
+
+            res.send(doctors);
+
+        });
+
+        app.delete('/doctor/:email', verifyJWT, verifyAdmin, async (req, res) => {
+
+            const email = req.params.email;
+
+            const filter = { email: email };
+
+            const result = await doctorCollection.deleteOne(filter);
 
             res.send(result);
 
